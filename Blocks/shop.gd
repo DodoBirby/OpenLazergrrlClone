9f4a6d6afc43fccf07c9f3a8c6a15b628b66bf86
DodoBirb <95009974+DodoBirby@users.noreply.github.com>
2024-12-08 18:34:01 +1100

    fix errors and configure linux preset

--- Blocks/shop.gd
+++ Blocks/shop.gd
@@ -3,7 +3,7 @@ extends Block
 
 @export var product: PackedScene
 
-func interact(player: Player) -> void:
+func interact(player: Player, _pos) -> void:
 	if player.team != team:
 		return
 	if player.held_block:
